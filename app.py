7f9f19022c3f8ae852a7acc4be0766fe8f958588
Corina Toweh <162394856+ctoweh@users.noreply.github.com>
2024-04-20 03:09:49 +0100

    flask

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
+import unittest
+import sys
 from flask import Flask
-
+sys.path.insert(0, './.myenv/bin/python.10')
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "I am almost a DevOps Engineer!"
-
-if __name__ == "__main__":
-    # Run the Flask app on host 0.0.0.0 and port 5000
-    app.run(use_reloader=True, host='0.0.0.0', port=5000)
+def hello_world():
+    return 'Hello World'
